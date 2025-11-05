73f9860687aec7ecc1f402e6cfa5710d26931cc7
Demid Greshnikov <demid.westhill@gmail.com>
2025-11-05 18:44:08 +0100

    update date validator

--- src/app/core/utils/validators/future-date.validator.ts
+++ src/app/core/utils/validators/future-date.validator.ts
@@ -1,10 +1,14 @@
-import { AbstractControl } from "@angular/forms";
+import { AbstractControl } from '@angular/forms';
 
 export const futureDateValidator = (control: AbstractControl) => {
+  if (!control.value) {
+    return null;
+  }
   const selectedDate = new Date(control.value);
+  selectedDate.setHours(0, 0, 0, 0);
   const today = new Date();
   today.setHours(0, 0, 0, 0);
-  if (control.value && selectedDate < today) {
+  if (selectedDate.getTime() < today.getTime()) {
     return { pastDate: true };
   }
   return null;
